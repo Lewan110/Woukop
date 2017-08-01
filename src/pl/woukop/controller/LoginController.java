ea52f63b35ee9d20ca1afa1783c112e14b1041de
Adrian <Lewan110@gmail.com>
2017-08-01 17:30:30 +0200

    final

--- src/pl/woukop/controller/LoginController.java
+++ src/pl/woukop/controller/LoginController.java
@@ -10,9 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
     private static final long serialVersionUID = 1L;
-        
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getUserPrincipal() != null) {
+ 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getUserPrincipal() != null) {
             response.sendRedirect(request.getContextPath() + "/");
         } else {
             response.sendError(403);
